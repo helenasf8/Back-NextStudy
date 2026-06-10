8a427252075e588a0a586e27c80ec1e8ce49f511
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-06-10 04:02:52 -0300

    feat: inclusão do uploader

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,12 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from uploader.router import router as uploader_router
+
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -23,6 +29,7 @@ router.register(r'materia', MateriaViewSet, basename='materia')
 urlpatterns = [
     path('admin/', admin.site.urls),
     # OpenAPI 3
+    path('api/media/', include(uploader_router.urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
         'api/doc/',
@@ -43,3 +50,5 @@ urlpatterns = [
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
