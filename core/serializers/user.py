af4969865bc3f3de56f0b20c7eb53af0044071c4
helenasf8 <helenadasilvafortunato.if@gmail.com>
2026-07-10 05:21:17 -0300

    feat: add default permission classes for REST framework; update UserRegistrationSerializer to create associated Cronograma on user creation

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -1,6 +1,5 @@
 from rest_framework.serializers import CharField, ModelSerializer, SlugRelatedField
-
-from core.models import User
+from core.models import User, Cronograma
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
@@ -13,7 +12,6 @@ class UserSerializer(ModelSerializer):
         required=False,
         write_only=True,
     )
-
     foto = ImageSerializer(required=False, read_only=True)
 
     class Meta:
@@ -41,4 +39,6 @@ class UserRegistrationSerializer(ModelSerializer):
         fields = ['id', 'email', 'name', 'password']
 
     def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
+        Cronograma.objects.create(usuario=user, nome='Meu Cronograma')
+        return user
